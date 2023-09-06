3296986f5d4f08735390e180ff0078fcd7f2dc96
Joshua Dahl <joshuadahl@nevada.unr.edu>
2023-09-06 14:55:15 -0700

    Core Properties
    Added a property class which acts like a reference but when it's value is read a function is called and when its value it set another function is called.
    Added some missing properties to the core variant types, and added a specialization of property for each that also makes all of its properties and methods available!

--- include/godot_cpp/variant/basis.hpp
+++ include/godot_cpp/variant/basis.hpp
@@ -32,6 +32,7 @@
 #define GODOT_BASIS_HPP
 
 #include <godot_cpp/classes/global_constants.hpp>
+#include <godot_cpp/classes/properties.hpp>
 #include <godot_cpp/variant/quaternion.hpp>
 #include <godot_cpp/variant/vector3.hpp>
 
@@ -50,6 +51,19 @@ struct _NO_DISCARD_ Basis {
 	_FORCE_INLINE_ Vector3 &operator[](int axis) {
 		return rows[axis];
 	}
+
+	Vector3 get_x() const { return rows[0]; }
+	Vector3 set_x(Vector3 value) { return rows[0] = value; }
+	godot::Property<Vector3, &Basis::get_x, &Basis::set_x> x() { return this; }
+
+	Vector3 get_y() const { return rows[1]; }
+	Vector3 set_y(Vector3 value) { return rows[1] = value; }
+	godot::Property<Vector3, &Basis::get_y, &Basis::set_y> y() { return this; }
+
+	Vector3 get_z() const { return rows[2]; }
+	Vector3 set_z(Vector3 value) { return rows[2] = value; }
+	godot::Property<Vector3, &Basis::get_z, &Basis::set_z> z() { return this; }
+
 
 	void invert();
 	void transpose();
@@ -313,6 +327,113 @@ real_t Basis::determinant() const {
 			rows[1][0] * (rows[0][1] * rows[2][2] - rows[2][1] * rows[0][2]) +
 			rows[2][0] * (rows[0][1] * rows[1][2] - rows[1][1] * rows[0][2]);
 }
+
+template <auto Getter, auto Setter> PROPERTY_TEMPLATE_CONSTRAINT(Getter, Setter)
+class Property<Basis, Getter, Setter> : public PropertyOperations<Property<Basis, Getter, Setter>> {
+    using T = Basis;
+    using Self = Property<Basis, Getter, Setter>;
+public:
+	PROPERTY_CORE(Getter, Setter)
+
+	GODOT_PROPERTY_WRAPPED_PROPERTY(Vector3*, rows, Self)
+
+	Vector3 get_x() const { return rows().get()[0]; }
+	Vector3 get_y() const { return rows().get()[1]; }
+	Vector3 get_z() const { return rows().get()[2]; }
+
+	Vector3 set_x(Vector3 value) {
+		auto temp = rows().get();
+		temp[0] = value;
+		rows().set(temp);
+		return temp;
+	}
+	Vector3 set_y(Vector3 value) {
+		auto temp = rows().get();
+		temp[1] = value;
+		rows().set(temp);
+		return temp;
+	}
+	Vector3 set_z(Vector3 value) {
+		auto temp = rows().get();
+		temp[2] = value;
+		rows().set(temp);
+		return temp;
+	}
+
+	GODOT_PROPERTY_WRAPPED_PROPERTY_NO_GET_SET(Vector3, x, Self);
+	GODOT_PROPERTY_WRAPPED_PROPERTY_NO_GET_SET(Vector3, y, Self);
+	GODOT_PROPERTY_WRAPPED_PROPERTY_NO_GET_SET(Vector3, z, Self);
+
+	GODOT_PROPERTY_WRAPPED_FUNCTION(invert, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(transpose, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(inverse, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(transposed, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(determinant, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(from_z, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(rotate_local, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(rotated_local, Self);
+	template<typename... Args> requires (getsetable<Self>) void rotate(Args... args) { auto temp = get(); temp.rotate(std::forward<Args>(args)...); set(temp); }
+	template<typename... Args> requires (!getsetable<Self> && getable<Self>) void rotate(Args... args) const { const auto temp = get(); temp.rotate(std::forward<Args>(args)...); }
+	template<typename... Args> requires (getsetable<Self>) auto rotated(Args... args) { auto temp = get(); auto ret = temp.rotated(std::forward<Args>(args)...); set(temp); return ret; }
+	template<typename... Args> requires (!getsetable<Self> && getable<Self>) auto rotated(Args... args) const { const auto temp = get(); auto ret = temp.rotated(std::forward<Args>(args)...); return ret; }
+	GODOT_PROPERTY_WRAPPED_FUNCTION(get_euler_normalized, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(get_rotation_axis_angle, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(get_rotation_axis_angle_local, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(get_rotation_quaternion, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(rotate_to_align, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(rotref_posscale_decomposition, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(get_euler, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(set_euler, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(from_euler, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(get_quaternion, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(set_quaternion, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(get_axis_angle, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(set_axis_angle, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(scale, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(scaled, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(scale_local, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(scaled_local, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(scale_orthogonal, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(scaled_orthogonal, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(make_scale_uniform, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(get_uniform_scale, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(get_scale, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(get_scale_abs, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(get_scale_local, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(set_axis_angle_scale, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(set_euler_scale, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(set_quaternion_scale, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(tdotx, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(tdoty, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(tdotz, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(is_equal_approx, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(is_finite, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(xform, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(xform_inv, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(is_orthogonal, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(is_diagonal, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(is_rotation, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(lerp, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(slerp, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(rotate_sh, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(set, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(set_columns, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(get_column, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(set_column, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(get_main_diagonal, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(set_zero, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(transpose_xform, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(orthonormalize, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(orthonormalized, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(orthogonalize, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(orthogonalized, Self);
+#ifdef MATH_CHECKS
+	GODOT_PROPERTY_WRAPPED_FUNCTION(is_symmetric, Self)
+#endif
+	GODOT_PROPERTY_WRAPPED_FUNCTION(diagonalize, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(looking_at, Self);
+	GODOT_PROPERTY_WRAPPED_FUNCTION(from_scale, Self);
+};
 
 } // namespace godot
 
